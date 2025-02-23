459c81ab2c449ce028ad7ca84c305b25836aa5a8
GarySimons <garycsimons@gmail.com>
2025-02-23 18:23:43 +0000

    Added HomeStyles.scss file. Adjusted marging on NavBar

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import "./HomeStyles.scss";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+
       <Footer />
     </div>
   );
