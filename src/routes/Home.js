6eed704e38e4fc6ca4cd1b231b3f78031afeed3e
GarySimons <garycsimons@gmail.com>
2025-02-26 06:06:30 +0000

    date and address added to home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -14,6 +14,11 @@ const Home = () => {
           <h1 className="name">Dylan</h1>
         </div>
         <p className="wedding">WEDDING</p>
+        <div className="home-date-container">
+          <p className="home-date">23.05.26</p>
+          <hr className="home-line" />
+          <p className="home-address">SOUTH STOKE BARN, ARUNDEL, WEST SUSSEX</p>
+        </div>
       </div>
 
       <Footer />
