72c7d8ed31168af72a6902334cefd74c0366bc63
GarySimons <garycsimons@gmail.com>
2025-03-03 07:10:13 +0000

    fake details added to Details page

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -23,8 +23,19 @@ const Details = () => {
           <p className="page-text link">
             <Link to="/location">Directions</Link>
           </p>
-          <p className="page-text bold">xxx</p>
-          <p className="page-text">xxx</p>
+          <p className="page-text bold">Time</p>
+          <p className="page-text">13.00</p>
+          <p className="page-text bold">Dress Code</p>
+          <p className="page-text">Smart Casual</p>
+          <p className="page-text bold">Accommodation</p>
+          <p className="page-text">Here are a few local options</p>
+          <p className="page-text link">
+            <Link to="/accommodation">Places to stay</Link>
+          </p>
+          <p className="page-text bold">Evening event</p>
+          <p className="page-text">19:00</p>
+          <p className="page-text bold">Carriages</p>
+          <p className="page-text">23:30</p>
         </div>
       </div>
       <Footer />
