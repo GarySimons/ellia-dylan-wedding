dde3f2ff4cfba85f0c57b4471ce46d7535d48953
GarySimons <garycsimons@gmail.com>
2025-03-04 07:07:47 +0000

    hotels json created. displaying on Accommodation page

--- src/routes/Accommodation.js
+++ src/routes/Accommodation.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import hotelsData from "../assets/data/hotels.json";
 
 const Accommodation = () => {
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    setHotels(hotelsData);
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="main-container">
-        <h1>Accommodation</h1>
+        <h1 className="page-heading">Accommodation</h1>
+        {hotels.map((hotel, index) => (
+          <div key={index}>
+            <p className="page-text bold">{hotel.name}</p>
+            <p className="page-text no-margin">{hotel.address}</p>
+            <p className="page-text">{hotel.phone}</p>
+            <a href={hotel.website} target="_blank" rel="noopener noreferrer">
+              <p className="page-text link">{hotel.websiteName}</p>
+            </a>
+            <br />
+          </div>
+        ))}
       </div>
       <Footer />
     </div>
