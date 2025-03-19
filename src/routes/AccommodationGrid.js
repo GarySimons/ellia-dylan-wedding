1883402bbf00935f28e185bea6c0b156676713fd
GarySimons <garycsimons@gmail.com>
2025-03-19 07:01:24 +0000

    Accommodation page with grid added.

--- src/routes/AccommodationGrid.js
+++ src/routes/AccommodationGrid.js
@@ -15,17 +15,18 @@ const AccommodationGrid = () => {
       <Navbar />
       <div className="main-container">
         <h1 className="page-heading">Accommodation Grid</h1>
-        {hotels.map((hotel, index) => (
-          <div key={index}>
-            <p className="page-text bold">{hotel.name}</p>
-            <p className="page-text no-margin">{hotel.address}</p>
-            <p className="page-text">{hotel.phone}</p>
-            <a href={hotel.website} target="_blank" rel="noopener noreferrer">
-              <p className="page-text link">{hotel.websiteName}</p>
-            </a>
-            <br />
-          </div>
-        ))}
+        <div className="hotel-grid">
+          {hotels.map((hotel, index) => (
+            <div key={index} className="hotel-card">
+              <p className="page-text bold">{hotel.name}</p>
+              <p className="page-text no-margin">{hotel.address}</p>
+              <p className="page-text">{hotel.phone}</p>
+              <a href={hotel.website} target="_blank" rel="noopener noreferrer">
+                <p className="page-text link">{hotel.websiteName}</p>
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
